51dad5e5cf87e8ebe4cf442ff799444cc0e8e47f
Igor Sh <igar15@yandex.ru>
2020-10-09 18:59:00 +0300

    add cart status functional part-2

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,7 +25,7 @@ export class ProductListComponent implements OnInit {
   previousKeyword: string = null;
 
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -109,7 +111,9 @@ export class ProductListComponent implements OnInit {
 
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
 
-    
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
+
   }
 
 }
